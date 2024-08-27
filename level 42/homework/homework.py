75bebb955e181f1b94fa339219160b22601c9e8e
Giorgigaluashvili <galu.galuashvili@gmail.com>
2024-08-27 23:11:27 +0400

    commit

---
+++ level 42/homework/homework.py
@@ -0,0 +1,19 @@
+#  fruits = ['Apple', 'Banana', 'Cherry', 'Date']
+
+# #გამოვიყენოთ join() ფუნქცია
+#  fruits_string = ', '.join(fruits)
+
+
+# print(fruits_string)  #გამოიტანს: "Apple, Banana, Cherry, Date"
+
+
+#-----------------------------
+
+
+# text = 'Hello, world!'
+
+# #შეიცვლება 'world' 'Python'-ით
+# new_text = text.replace('world', 'Python')
+
+# print(new_text)  #გამოიტანს: "Hello, Python!"
+
